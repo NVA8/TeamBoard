509544a22f52035cb0cc1eb1282b1e232bc40628
NVA8 <95127908+NVA8@users.noreply.github.com>
2025-11-01 14:45:05 +0300

    Initial commit — fresh start

---
+++ Team boardTests/Team_boardTests.swift
@@ -0,0 +1,17 @@
+//
+//  Team_boardTests.swift
+//  Team boardTests
+//
+//  Created by Валерий Никитин on 01.11.2025.
+//
+
+import Testing
+@testable import Team_board
+
+struct Team_boardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
